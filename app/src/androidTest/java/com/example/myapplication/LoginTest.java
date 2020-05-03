f30724effc9929bd4949e0891119f836ffae288b
cassius98 <61149196+cassius98@users.noreply.github.com>
2020-05-03 22:16:20 +0200

    updated test and login

---
+++ app/src/androidTest/java/com/example/myapplication/LoginTest.java
@@ -0,0 +1,39 @@
+package com.example.myapplication;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.internal.runner.junit4.statement.UiThreadStatement.runOnUiThread;
+
+@RunWith(AndroidJUnit4.class)
+public class LoginTest {
+
+
+
+
+    @Rule
+    public ActivityTestRule<Login> activityTestRule = new ActivityTestRule<>(Login.class);
+
+
+
+    @Test
+    public void tryLoginAsStudent(){
+        try {
+            runOnUiThread(() -> {
+                activityTestRule.getActivity().findViewById(R.id.signin).performClick();
+                activityTestRule.getActivity().a.setText("1627982");
+                activityTestRule.getActivity().b.setText("wrong pass");
+                activityTestRule.getActivity().findViewById(R.id.signin).performClick();
+            });
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+    }
+
+
+
+}
--- app/src/main/java/com/example/myapplication/Login.java
+++ app/src/main/java/com/example/myapplication/Login.java
@@ -1,7 +1,5 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.ContentValues;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,6 +7,8 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity implements View.OnClickListener {
 
