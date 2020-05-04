9b8702551ec201a350d3fd69ded7dec366bfc689
cassius98 <61149196+cassius98@users.noreply.github.com>
2020-05-04 10:37:07 +0200

    homepage

---
+++ app/src/androidTest/java/com/example/myapplication/HomepageTest.java
@@ -0,0 +1,62 @@
+package com.example.myapplication;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.internal.runner.junit4.statement.UiThreadStatement.runOnUiThread;
+import static org.junit.Assert.*;
+
+
+@RunWith(AndroidJUnit4.class)
+public class HomepageTest {
+
+    @Rule
+    public ActivityTestRule<Homepage> activityTestRule = new ActivityTestRule<>(Homepage.class);
+
+    @Test
+    public void gedaddbook(){
+        try {
+            runOnUiThread(() -> {
+                activityTestRule.getActivity().findViewById(R.id.addbook).performClick();
+            });
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+    }
+
+
+
+    @Test
+    public void geletebookssss(){
+        try {
+            runOnUiThread(() -> {
+                activityTestRule.getActivity().findViewById(R.id.deletebook).performClick();
+            });
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+    }
+
+
+
+    @Test
+    public void justrunavoid(){
+        try {
+            runOnUiThread(() -> {
+                activityTestRule.getActivity().findViewById(R.id.searchbook).performClick();
+
+            });
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+    }
+
+
+
+
+
+}
