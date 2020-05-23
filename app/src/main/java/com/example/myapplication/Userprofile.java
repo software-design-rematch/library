288da94c327bd9062ec06ca6643652f7fd89d3a6
TALIFHANIN <36076744+TALIFHANIN@users.noreply.github.com>
2020-05-23 23:28:54 +0200

    user profile

--- app/src/main/java/com/example/myapplication/Userprofile.java
+++ app/src/main/java/com/example/myapplication/Userprofile.java
@@ -2,10 +2,16 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Userprofile extends AppCompatActivity implements View.OnClickListener{
 
@@ -23,6 +29,56 @@ public class Userprofile extends AppCompatActivity implements View.OnClickListen
 
         bt=(Button)findViewById(R.id.back);
         bt.setOnClickListener(this);
+
+
+        final TextView t = (TextView)findViewById(R.id.displayinfo);
+        ContentValues params = new ContentValues();
+        params.put("email",ems);
+
+        AsyncHTTPPost asyncHTTPPost = new AsyncHTTPPost("http://192.168.43.68/userinfo.php",params) {
+            @Override
+            protected void onPostExecute(String output) {
+
+                JSONArray T = null;
+                try{
+                    T = new JSONArray(output);
+                    int a =T.length();
+                    String Arr1[] = new String[a];
+
+
+                    for(int i =0;i<T.length();i++){
+                        String some="";
+                        JSONObject Aw =(JSONObject)T.get(i);
+
+                        some = " "+"first names  :  "+Aw.get("name")+"\n"+"\n"+"\n"+"\n"+"  "+"surname  :  "+" "+Aw.get("surname")+"\n"+"\n"+"\n"+"\n"+"  "
+                                +" "+"email  :  "+" "+Aw.get("email") +"\n"+"\n"+"\n"+"\n"+"  "+"gender  : "+Aw.get("gender")+"\n"+"\n"+"\n"+"\n";
+                        Arr1[i]=some;
+                    }
+
+                    String fn="";
+
+                    for(int i=0;i<Arr1.length;i++){
+
+                        fn = fn + Arr1[i]+"\n";
+                    }
+
+                    t.setText(fn);
+
+                }
+                catch(JSONException k){k.printStackTrace();}
+
+
+
+            }
+        };
+        asyncHTTPPost.execute();
+
+
+
+
+
+
+
 
 
 
