fb863acb76c97d82bbbae8be9356db874a48a9d5
cassius98 <thangza98c@gmail.com>
2020-05-28 20:10:21 +0200

    updates

--- app/src/main/java/com/example/myapplication/Homepage.java
+++ app/src/main/java/com/example/myapplication/Homepage.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,6 +11,11 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.zip.Inflater;
 
@@ -26,6 +32,76 @@ public class Homepage extends AppCompatActivity implements View.OnClickListener 
 
         Intent intent =getIntent();
         ems =intent.getStringExtra("email");
+
+
+
+
+
+
+
+
+
+
+
+        final TextView t = (TextView)findViewById(R.id.displayinfo);
+        ContentValues params = new ContentValues();
+        params.put("email",ems);
+
+        AsyncHTTPPost asyncHTTPPost = new AsyncHTTPPost("http://192.168.43.68/userhistory.php",params) {
+            @Override
+            protected void onPostExecute(String output) {
+
+                JSONArray T = null;
+                try{
+                    T = new JSONArray(output);
+                    int a =T.length();
+                    String Arr1[] = new String[a];
+
+
+                    for(int i =0;i<T.length();i++){
+                        String some="";
+                        JSONObject Aw =(JSONObject)T.get(i);
+
+                        some = " "+"email  :  "+" "+Aw.get("email")+" "+"\n"+"\n"+"book code  :  "+Aw.get("isbn")+"\n"+"\n"+"  "+"time  :  "+" "+Aw.get("returntime");
+                        Arr1[i]=some;
+                    }
+
+                    String fn="";
+
+                    for(int i=0;i<Arr1.length;i++){
+
+                        fn = fn + Arr1[i]+"\n";
+                    }
+
+                    t.setText(fn);
+
+                }
+                catch(JSONException k){k.printStackTrace();}
+
+
+
+            }
+        };
+        asyncHTTPPost.execute();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     }
