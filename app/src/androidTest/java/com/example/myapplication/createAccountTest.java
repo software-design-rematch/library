5731ba325390bb384cd2f6a0dd5533d79de8ca34
cassius98 <thangza98c@gmail.com>
2020-05-29 13:20:00 +0200

    updates

--- app/src/androidTest/java/com/example/myapplication/createAccountTest.java
+++ app/src/androidTest/java/com/example/myapplication/createAccountTest.java
@@ -18,27 +18,27 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class createAccountTest {
 
-    private createAccount createA;
+    //private createAccount createA;
 
 
     @Rule
     public ActivityTestRule<createAccount> activityTestRule = new ActivityTestRule<>(createAccount.class);
 
 
-    @Before
-    public void setup(){
-        createA = new createAccount();
-
-    }
-
-
 
 
     @Test
-    public void testVerifyinput(){
-        int res = createA.verifyinput("cassius","surname","c@gmail.com","aa","aa","male");
-       assertEquals("failed",1,res);
-
+    public void dummyFunctionWithParams(){
+        try{
+            runOnUiThread(() -> {
+                activityTestRule.getActivity().verifyinput("cassius","surname","c@gmail.com","aa","aa","male");
+            });
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
     }
 
+
+
+    
 }
