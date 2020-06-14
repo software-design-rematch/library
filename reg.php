2ffb68fca6f06709db835d0e868c0ca67c855f86
TALIFHANIN <36076744+TALIFHANIN@users.noreply.github.com>
2020-06-14 13:18:30 +0200

    php file for registration

---
+++ reg.php
@@ -0,0 +1,53 @@
+<?php
+
+$servername ="192.168.43.68";
+$user ="root";
+$pass="";
+$dbname="project";
+
+$conn = mysqli_connect($servername,$user,$pass,$dbname);
+
+if($conn){
+	echo ("done,");
+	
+}
+else{
+	echo ("failed");
+
+}
+
+
+if($_SERVER['REQUEST_METHOD']=='POST'){
+	
+	$name = $_REQUEST['name'];
+	$surname  = $_REQUEST['surname'];
+ 	$email = $_REQUEST['email'];
+	$gender = $_REQUEST['gender'];
+ 	$password  = $_REQUEST['password'];
+	$passs =md5($password);
+	
+	$query = "INSERT INTO `registration`(`name`, `surname`, `email`, `password`, `gender`) VALUES ('$name', '$surname', '$email', '$passs', '$gender')";
+
+	
+	
+	if(mysqli_query($conn,$query)){
+		
+			echo ("registration successful");
+		
+	}
+	else{
+		echo ("email address already exist");
+		
+	}
+	
+}
+	else{
+		echo ("registration not suuccessful");
+		
+	}
+
+
+
+
+
+?>
