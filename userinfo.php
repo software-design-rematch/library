98c9ba8773a0833631a1f1c8d1228bda3629f551
TALIFHANIN <36076744+TALIFHANIN@users.noreply.github.com>
2020-06-14 13:31:57 +0200

    php file for userinfo

---
+++ userinfo.php
@@ -0,0 +1,20 @@
+<?php
+$username = "root";
+$password = "";
+$database = "project";
+
+
+$link = mysqli_connect("ipaddress", $username, $password, $database);
+
+$email =$_REQUEST["email"];
+$output=array();
+
+
+if ($result = mysqli_query($link, "SELECT * FROM registration where email='$email'")) {
+while ($row=$result->fetch_assoc()){
+$output[]=$row;
+}
+}
+mysqli_close($link);
+echo json_encode($output);
+?>
